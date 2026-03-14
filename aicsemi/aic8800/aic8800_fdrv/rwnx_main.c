f579b5fbd186fdc90ece355b1c5ef94e11cebfeb
queenkjuul <queenkjuul@pm.me>
2026-03-14 01:35:16 +0000

    net: wireless: aic8800: modernize cfg80211_ch_switch_notify

--- aicsemi/aic8800/aic8800_fdrv/rwnx_main.c
+++ aicsemi/aic8800/aic8800_fdrv/rwnx_main.c
@@ -782,7 +782,9 @@ static void rwnx_csa_finish(struct work_struct *ws)
 		} else
 			rwnx_txq_vif_stop(vif, RWNX_TXQ_STOP_CHAN, rwnx_hw);
 		spin_unlock_bh(&rwnx_hw->cb_lock);
-#if (LINUX_VERSION_CODE >= HIGH_KERNEL_VERSION3)
+#if (LINUX_VERSION_CODE > KERNEL_VERSION(6, 19, 0))
+		cfg80211_ch_switch_notify(vif->ndev, &csa->chandef, 0);
+#elif (LINUX_VERSION_CODE >= HIGH_KERNEL_VERSION3)
 		cfg80211_ch_switch_notify(vif->ndev, &csa->chandef, 0, 0);
 #elif (LINUX_VERSION_CODE >= HIGH_KERNEL_VERSION)
 		cfg80211_ch_switch_notify(vif->ndev, &csa->chandef, 0);
